ffac2061283ce5d7030cd82382c9b6e440a97867
artespoon <artespoon@gmail.com>
2018-11-20 00:30:52 +0100

    Added support and tests to compute properly not overlapping orders and bigger numbers. Added Math.floor to average waiting time. Naming refactoring.

--- src/main/scala/OrdersProcessor.scala
+++ src/main/scala/OrdersProcessor.scala
@@ -9,32 +9,46 @@ case class CustomerOrder(timeToOrder: Int, timeToCook: Int, waitingTime: Long = 
 
 object OrdersProcessor {
 
-    def getAvgWaitingTime(customerOrders: List[CustomerOrder]):Int = {
-      val sortedList = customerOrders.sortBy(c => c.timeToOrder)
+    def getAvgWaitingTime(customerOrders: List[CustomerOrder]):Long = {
+      val sortedList = customerOrders.sortBy(_.timeToOrder)
 
       @tailrec
       def rec(lastProcessedCustomerOrderOpt: Option[CustomerOrder], co: List[CustomerOrder], waitingTimes: List[Long]): List[Long] = {
         (lastProcessedCustomerOrderOpt, co) match {
           case (_, Nil) => waitingTimes
           case (Some(lastProcessedCustomerOrder), customerOrder :: Nil) =>
-            val customerOrderWaitingTime = lastProcessedCustomerOrder.timeToOrder + lastProcessedCustomerOrder.waitingTime - customerOrder.timeToOrder + customerOrder.timeToCook
+            val customerOrderWaitingTime = computeWaitingTime(Some(lastProcessedCustomerOrder), customerOrder)
             rec(None, Nil, customerOrderWaitingTime :: waitingTimes)
           case (Some(lastProcessedCustomerOrder), customerOrders @ head :: tail) =>
-            val optimizedOrders = customerOrders.filter(_.timeToOrder <= lastProcessedCustomerOrder.waitingTime)
-              .map(co => CustomerOrder(co.timeToOrder, co.timeToCook, lastProcessedCustomerOrder.timeToOrder + lastProcessedCustomerOrder.waitingTime - co.timeToOrder + co.timeToCook))
-              .sortBy(_.waitingTime)
-            rec(Some(optimizedOrders.head), optimizedOrders.tail, optimizedOrders.head.waitingTime :: waitingTimes)
+            val overlappingCustomerOrders = customerOrders.filter(_.timeToOrder <= lastProcessedCustomerOrder.waitingTime)
+            val optimizedCustomerOrders = overlappingCustomerOrders match {
+              case Nil => CustomerOrder(customerOrders.head.timeToOrder, customerOrders.head.timeToCook, customerOrders.head.timeToCook) :: customerOrders.tail
+              case _ => overlappingCustomerOrders.map(co => CustomerOrder(co.timeToOrder, co.timeToCook, computeWaitingTime(Some(lastProcessedCustomerOrder), co))).sortBy(_.waitingTime)
+            }
+            rec(Some(optimizedCustomerOrders.head), optimizedCustomerOrders.tail, optimizedCustomerOrders.head.waitingTime :: waitingTimes)
           case (None, customerOrder :: tail) =>
-            val waitingTime = customerOrder.timeToOrder + customerOrder.timeToCook
-            rec(Some(CustomerOrder(customerOrder.timeToOrder, customerOrder.timeToCook, waitingTime)), tail, waitingTime :: waitingTimes)
+            val customerOrderWaitingTime = computeWaitingTime(None, customerOrder)
+            rec(Some(CustomerOrder(customerOrder.timeToOrder, customerOrder.timeToCook, customerOrderWaitingTime)), tail, customerOrderWaitingTime :: waitingTimes)
         }
       }
+
       val waitingTimes = rec(None, sortedList, List.empty[Long])
       waitingTimes match {
         case Nil => 0
-        case _ => (sumAsLong(waitingTimes) / sortedList.size.toLong).toInt
+        case _ => Math.floorDiv(sumAsLong(waitingTimes), sortedList.size.toLong)
       }
     }
+
+  private def computeWaitingTime(lastProcessedCustomerOrderOpt: Option[CustomerOrder], currentCustomerOrder: CustomerOrder):Long = {
+    lastProcessedCustomerOrderOpt match {
+      case None =>
+        currentCustomerOrder.timeToOrder.toLong + currentCustomerOrder.timeToCook.toLong
+      case Some(lastProcessedCustomerOrder) if lastProcessedCustomerOrder.waitingTime < currentCustomerOrder.timeToOrder =>
+        currentCustomerOrder.timeToCook
+      case Some(lastProcessedCustomerOrder) =>
+        lastProcessedCustomerOrder.timeToOrder + lastProcessedCustomerOrder.waitingTime - currentCustomerOrder.timeToOrder.toLong + currentCustomerOrder.timeToCook.toLong
+    }
+  }
 
   private def sumAsLong(list: List[Long]): Long = list.foldLeft(0L)((acc, e) => acc + e)
 }
--- src/test/scala/OrdersProcessorSpec.scala
+++ src/test/scala/OrdersProcessorSpec.scala
@@ -10,17 +10,33 @@ class OrdersProcessorSpec extends Specification {
       OrdersProcessor.getAvgWaitingTime(List()) must_== 0
     }
 
-    "Test with proper list with customer orders" >> {
+    "Test with proper list of customer orders" >> {
       OrdersProcessor.getAvgWaitingTime(List(CustomerOrder(0,3), CustomerOrder(1,9), CustomerOrder(2,6))) must_== 9
       OrdersProcessor.getAvgWaitingTime(List(CustomerOrder(0,3), CustomerOrder(1,9), CustomerOrder(2,5))) must_== 8
     }
 
-    "Test with list in wrong order " >> {
+    "Test with list of customers in wrong order " >> {
       OrdersProcessor.getAvgWaitingTime(List(CustomerOrder(1,9), CustomerOrder(2,6), CustomerOrder(0,3))) must_== 9
     }
 
     "Test with all orders at the same time " >> {
       OrdersProcessor.getAvgWaitingTime(List(CustomerOrder(0,3), CustomerOrder(0,3), CustomerOrder(0,3))) must_== 6
+    }
+
+    "Test with NOT overlapping orders" >> {
+      OrdersProcessor.getAvgWaitingTime(List(CustomerOrder(0,1), CustomerOrder(10,3))) must_== 2
+    }
+
+    "Test with NOT overlapping orders 1" >> {
+      OrdersProcessor.getAvgWaitingTime(List(CustomerOrder(0,1), CustomerOrder(10,2), CustomerOrder(20,3))) must_== 2
+    }
+
+    "Test with time to order with Int max value" >> {
+      OrdersProcessor.getAvgWaitingTime(List(CustomerOrder(0,1), CustomerOrder(Int.MaxValue,1))) must_== 1
+    }
+
+    "Test with time to order with Int max value 1" >> {
+      OrdersProcessor.getAvgWaitingTime(List(CustomerOrder(0,1), CustomerOrder(3, Int.MaxValue), CustomerOrder(Int.MaxValue,1))) must_== 715827884 //Math.floorDiv(1L + Int.MaxValue + 4L, 3L)
     }
   }
 }
